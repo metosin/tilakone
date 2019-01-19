758c4f3e64e2590dfdd63395c3787f35ba648e80
Jarppe <jarppe@metosin.fi>
2019-01-19 20:52:09 +0200

    Targeting 0.0.5-SNAPSHOT

--- modules/core/project.clj
+++ modules/core/project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/tilakone.core "0.0.4"
+(defproject metosin/tilakone.core "0.0.5-SNAPSHOT"
   :description "Minimal finite state machine library"
   :url "https://github.com/metosin/tilakone"
   :scm {:name "git", :url "https://github.com/metosin/tilakone"}
--- modules/schema/project.clj
+++ modules/schema/project.clj
@@ -1,8 +1,8 @@
-(defproject metosin/tilakone.schema "0.0.4"
+(defproject metosin/tilakone.schema "0.0.5-SNAPSHOT"
   :description "Minimal finite state machine library"
   :url "https://github.com/metosin/tilakone"
   :scm {:name "git", :url "https://github.com/metosin/tilakone"}
-  :dependencies [[metosin/tilakone.core "0.0.4"]
+  :dependencies [[metosin/tilakone.core "0.0.5-SNAPSHOT"]
                  [prismatic/schema "1.1.9"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
